6fde993a8e6ae18e0ce9d0b79ce272a294388186
Mateus Lima <126008846+MateusLimadev@users.noreply.github.com>
2023-09-19 18:32:24 -0300

    beta1

---
+++ script.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    document.getElementsByClassName('menu-lateral')[0].style.width = '250px';
+}
+
+function fecharMenu() {
+    document.getElementsByClassName('menu-lateral')[0].style.width = '0';
+}
+
